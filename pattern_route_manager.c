4e112ff10ae9e6f185e009999c25849b2461b5a6
Armmanoj <armj2357@gmail.com>
2024-04-14 04:30:44 +0530

    prototypes

--- pattern_route_manager.c
+++ pattern_route_manager.c
@@ -4,10 +4,11 @@ void pattern_manager(int M, int N, int* Gx,int* Gy, <vector<vector<int>> bx1, <v
     float T = 1000;
     int count = bx1.size();
     <vector<vector<int>> cost(count);
-    float tot_cost = 0;
+    float tot_cost = std::numeric_limits<int>::max();
+    float new_tot_cost = 0;
     for (int i=0; i<count;i++){
         cost[i] = std::vector<int>(count, std::numeric_limits<int>::max());
-        pattern_route(Gx,Gy,M,N,bx1[0],by1[0],bx2[0],by2[0], bx1[0].size(),cost[0]);
+        new_tot_cost = pattern_route(Gx,Gy,M,N,bx1[0],by1[0],bx2[0],by2[0], bx1[0].size(),cost[0]);
     }
     
     return;
